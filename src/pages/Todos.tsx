9259faee44f265a863be36950713312b97abf943
unknown <devRezaulKarim>
2024-09-26 11:58:30 +0600

    FEAT: Add filter functionality based on status
    **Add `Status.tsx` component for rendering the status options.
    **Add `label.tsx` component from shadcnUi
    **Add `radio-group.tsx` component from shadcnUi

--- src/pages/Todos.tsx
+++ src/pages/Todos.tsx
@@ -1,47 +1,78 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import AddTodo from "../components/custom-components/todo/AddTodo";
 import TodoContainer from "../components/custom-components/todo/TodoContainer";
 import AppTitle from "@/components/shared/AppTitle";
 import BackButton from "@/components/shared/BackButton";
+import Status from "@/components/custom-components/todo/Status";
 
 interface Todo {
   task: string;
   _id: number;
-  status: "pending" | "done";
+  status: "pending" | "completed";
 }
 
 const Todos = () => {
+  const [allTodos, setAllTodos] = useState<Todo[]>([
+    {
+      task: "Demo task",
+      _id: 1,
+      status: "pending",
+    },
+  ]);
   const [todos, setTodos] = useState<Todo[]>([]);
+  const [selectedStatus, setSelectedStatus] = useState<string>("all");
 
   const addTodo = (task: string) => {
-    setTodos((pre) => [...pre, { task, _id: Date.now(), status: "pending" }]);
+    setAllTodos((pre) => [
+      ...pre,
+      { task, _id: Date.now(), status: "pending" },
+    ]);
   };
 
   const handleTaskDone = (_id: number) => {
-    setTodos((prevTask) =>
+    setAllTodos((prevTask) =>
       prevTask.map((task) =>
         task._id === _id
-          ? { ...task, status: task.status === "done" ? "pending" : "done" }
+          ? {
+              ...task,
+              status: task.status === "completed" ? "pending" : "completed",
+            }
           : task
       )
     );
   };
 
   const handleUpdateTask = (task: Todo) => {
-    setTodos((prevTasks) =>
+    setAllTodos((prevTasks) =>
       prevTasks.map((prevTask) => (prevTask._id === task._id ? task : prevTask))
     );
   };
 
   const handleDeleteTask = (_id: number) => {
-    setTodos((prevTasks) => prevTasks.filter((task) => task._id !== _id));
+    setAllTodos((prevTasks) => prevTasks.filter((task) => task._id !== _id));
   };
+
+  useEffect(() => {
+    if (selectedStatus === "all") {
+      setTodos(allTodos);
+    }
+    if (selectedStatus === "pending") {
+      setTodos(allTodos.filter((todo) => todo.status === "pending"));
+    }
+    if (selectedStatus === "completed") {
+      setTodos(allTodos.filter((todo) => todo.status === "completed"));
+    }
+  }, [allTodos, selectedStatus]);
 
   return (
     <div className="w-full max-w-[800px] mx-auto border border-gray-500 rounded-lg p-2 md:p-8 relative">
       <BackButton />
       <AppTitle title="todo app" />
       <AddTodo addTodo={addTodo} />
+      <Status
+        selectedStatus={selectedStatus}
+        setSelectedStatus={setSelectedStatus}
+      />
       {todos.length > 0 && (
         <TodoContainer
           todos={todos}

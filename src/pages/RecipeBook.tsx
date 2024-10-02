639997ef371c86035619fbd1ec1fe0c02ae01514
unknown <devRezaulKarim>
2024-10-02 15:28:46 +0600

    FEAT: Add recipe api in recipe book app

--- src/pages/RecipeBook.tsx
+++ src/pages/RecipeBook.tsx
@@ -1,9 +1,27 @@
 import AppTitle from "@/components/shared/AppTitle";
 import BackButton from "@/components/shared/BackButton";
+import SmallForm from "@/components/shared/SmallForm";
+import { Button } from "@/components/ui/button";
+import { Recipe } from "@/types/Types";
+import { useEffect, useState } from "react";
 import { useLocation } from "react-router-dom";
 
 const RecipeBook = () => {
+  const [recipeFor, setRecipeFor] = useState<string>("");
+  const [recipes, setRecipes] = useState<Recipe[]>([]);
   const location = useLocation();
+
+  useEffect(() => {
+    console.log(recipes);
+  }, [recipes]);
+
+  useEffect(() => {
+    const url = `https://dummyjson.com/recipes/search?q=${recipeFor}&limit=50`;
+    fetch(url)
+      .then((res) => res.json())
+      .then((res) => setRecipes(res.recipes));
+  }, [recipeFor]);
+
   return (
     <div
       className={`w-full max-w-[800px] mx-auto border-gray-500 rounded-lg p-2 md:p-8 relative ${
@@ -13,6 +31,22 @@ const RecipeBook = () => {
       <BackButton />
 
       <AppTitle title="Recipe Book" />
+      <div className="flex gap-2">
+        <SmallForm
+          buttonText="Search"
+          placeholder="Chicken"
+          dispatchSubmit={setRecipeFor}
+        />
+        {recipeFor && (
+          <Button
+            onClick={() => setRecipeFor("")}
+            variant="outline"
+            className="text-red-500 hover:text-red-600 border-red-500 uppercase"
+          >
+            Reset
+          </Button>
+        )}
+      </div>
     </div>
   );
 };
--- src/types/Types.tsx
+++ src/types/Types.tsx
@@ -91,3 +91,22 @@ export interface Weather {
 export interface WeatherCardProps {
   weather: Weather;
 }
+
+export interface Recipe {
+  id: number;
+  name: string;
+  ingredients: string[];
+  instructions: string[];
+  prepTimeMinutes: number;
+  cookTimeMinutes: number;
+  servings: number;
+  difficulty: string;
+  cuisine: string;
+  caloriesPerServing: number;
+  tags: string[];
+  userId: number;
+  image: string;
+  rating: number;
+  reviewCount: number;
+  mealType: string[];
+}
